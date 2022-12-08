8e92d71ed96736b91e7dbf5c77418d29e44985f7
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-12-08 23:40:21 +0300

    first

---
+++ client/src/components/answer/Answer.js
@@ -0,0 +1,24 @@
+import "../answer/Answer.css";
+
+const Answer = ({
+  answerText,
+  index,
+  onSelectedAnswer,
+  currentAnswer,
+  correctAnswer,
+}) => {
+  const letterMapping = ["A", "B", "C", "D"];
+  const selectedAnswer = currentAnswer === answerText;
+  const selectClass = selectedAnswer ? "selected-answer" : "";
+  const disableClass = currentAnswer ? "disabled-answer" : "";
+
+  return (
+    <article
+      className={`answer ${selectClass} ${disableClass}`}
+      onClick={() => onSelectedAnswer(answerText)}
+    >
+      {letterMapping[index]}. {answerText}
+    </article>
+  );
+};
+export default Answer;
